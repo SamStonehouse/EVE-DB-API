c9bd9c7e52f034fad1472b7efa342219c7a72eb5
Sam Stonehouse <sgstonehouse@googlemail.com>
2014-04-08 20:32:51 +0100

    Added corp activity DB queries

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 // server.js (Express 4.0)
 var express        = require('express');
 var morgan         = require('morgan');
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
-var app            = express();
+var db             = require('./lib/mysql/mysql-driver.js');
+var eveApi         = require('./lib/eveapi.js')(db);
 
-var db = require('./lib/mysql/mysql-driver.js');
-var eveApi = require('./lib/eveapi.js')(db);
+var app            = express();
 
 var port = 8080;
 
-app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
-app.use(bodyParser());
-app.use(methodOverride());
 
 eveApi.setupRoutes(app);
 
