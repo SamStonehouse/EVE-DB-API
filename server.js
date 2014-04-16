ff1ec4101dd78d84ec825fd1867755f5f7de890a
Sam Stonehouse <sgstonehouse@googlemail.com>
2014-04-16 21:53:25 +1000

    Added cache skeleton, refactored the way Mysql DB queries are made and changed Agent queries, other queries to follow

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 // server.js (Express 4.0)
 var express        = require('express');
 var morgan         = require('morgan');
-var db             = require('./lib/mysql/mysql-controller.js');
+var cache          = require('./lib/db-cache/cache-controller');
+var db             = require('./lib/database/db-controller')(cache);
 var eveApi         = require('./lib/eveapi.js')(db);
 
 var app            = express();
 
 var port = 8080;
 
+//Setup logger
 app.use(morgan('dev'));
 
+//Connect to database & cache
+db.connect();
+
+//Setup API routes
 eveApi.setupRoutes(app);
 
+//Listen on required port
 app.listen(port);
+
+
 console.log('Listening on port ' + port);
