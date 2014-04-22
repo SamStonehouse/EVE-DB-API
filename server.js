1b63a168cca696f6aca512e9bf2e18fbbbcea47d
Sam Stonehouse <sgstonehouse@googlemail.com>
2014-04-22 20:19:10 +1000

    Files organised, API routes partly refactored, Memcache attempt, didn't go well

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express        = require('express');
 var morgan         = require('morgan');
 var cache          = require('./lib/cache/cache-controller');
 var db             = require('./lib/database/db-controller')(cache);
-var api            = require('./lib/eveapi')(db);
+var api            = require('./lib/api/api-controller');
 
 var app            = express();
 
@@ -14,9 +14,10 @@ app.use(morgan('dev'));
 
 //Connect to database & cache
 db.connect();
+cache.connect();
 
 //Setup API routes
-api.setupRoutes(app);
+api(db, app);
 
 //Listen on required port
 app.listen(port);
