1379ac9627f0a71ef50e8e6a753cd4d047ad564c
Sam Stonehouse <sgstonehouse@googlemail.com>
2014-04-27 22:14:30 +1000

    Combined effects and attribute type queries into one query

---
+++ fittingLayout.js
@@ -0,0 +1,26 @@
+fitting {
+	charater: {
+		skills: {
+
+		},
+		implants: {
+
+		},
+		drugs: {
+
+		}
+	},
+	loadout: {
+		meta: {
+			name:
+			description:
+			author:
+		},
+		ship {
+
+		},
+		modules: {
+
+		}
+	}
+}
